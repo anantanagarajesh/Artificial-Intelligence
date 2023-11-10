da639c095c30ff356dc39c294fbf87b6b80ebeaa
M ANANTA NAGA RAJESH <98514984+anantanagarajesh@users.noreply.github.com>
2023-11-10 10:10:37 +0530

    Add files via upload

---
+++ Program-3=Pattern.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the number: "))
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(i,end=' ')
+    print('\n')
+    
+    
+m = int(input("Enter the number: "))
+for i in range(1,m+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+    print('\n')
